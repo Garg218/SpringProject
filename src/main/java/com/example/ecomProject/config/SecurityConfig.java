d172fe253d21d2f530986d6b9372270fc52cb1e2
Gar218 <priya21814@gmail.com>
2026-02-25 00:55:51 +0530

    adding SecurityConfig

--- src/main/java/com/example/ecomProject/config/SecurityConfig.java
+++ src/main/java/com/example/ecomProject/config/SecurityConfig.java
@@ -1,12 +1,17 @@
 package com.example.ecomProject.config;
 
 
+import com.example.ecomProject.service.CustomUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -14,6 +19,8 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private CustomUserDetails customUserDetails;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http)
@@ -27,6 +34,15 @@ public class SecurityConfig {
 
 
         return http.build();
+    }
+
+
+    @Bean
+    public AuthenticationProvider authenticationProvider()
+    {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider(customUserDetails);
+        daoAuthenticationProvider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        return daoAuthenticationProvider;
     }
 }
 
